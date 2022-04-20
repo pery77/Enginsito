6a74729d29b8a7dca3c4d057f1b590aa979549ce
pery77 <el-pery@hotmail.com>
2022-04-21 01:12:02 +0200

    game destructor fix

--- Game.h
+++ Game.h
@@ -1,7 +1,11 @@
 #include "engine.h"
 class Game : public Engine{
+
+public:
     virtual void Init() override;
     virtual void Tick() override;		 
 	virtual void Draw() override;
 	virtual void OverDraw() override;
+
+    virtual ~Game(){};
 };
