a5a255df660869643dc8f0647c73b030d2b2398d
pery77 <el-pery@hotmail.com>
2023-02-02 23:17:17 +0100

    added wren

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,62 @@
 #include <iostream>
 #include "FileWatcher.h"
 
+#include "wren.h"
+
+void writeFn(WrenVM* vm, const char* text) {
+  printf("%s", text);
+}
+void errorFn(WrenVM* vm, WrenErrorType errorType,
+             const char* module, const int line,
+             const char* msg)
+{
+  switch (errorType)
+  {
+    case WREN_ERROR_COMPILE:
+    {
+      printf("[%s line %d] [Error] %s\n", module, line, msg);
+    } break;
+    case WREN_ERROR_STACK_TRACE:
+    {
+      printf("[%s line %d] in %s\n", module, line, msg);
+    } break;
+    case WREN_ERROR_RUNTIME:
+    {
+      printf("[Runtime Error] %s\n", msg);
+    } break;
+  }
+}
+
+
 int main(int argc, char *argv[]){
 
+    WrenConfiguration wConfig;
+    wrenInitConfiguration(&wConfig);
+        wConfig.writeFn = &writeFn;
+        wConfig.errorFn = &errorFn;
+
+    WrenVM* vm = wrenNewVM(&wConfig);
+    const char* module = "main";
+    const char* script = "System.print(\"I am running in a VM!\")";
+
+    WrenInterpretResult result = wrenInterpret(vm, module, script);
+
+    switch (result) {
+        case WREN_RESULT_COMPILE_ERROR:
+        { printf("Compile Error!\n"); } break;
+        case WREN_RESULT_RUNTIME_ERROR:
+        { printf("Runtime Error!\n"); } break;
+        case WREN_RESULT_SUCCESS:
+        { printf("Success!\n"); } break;
+    }
+    wrenFreeVM(vm);
+
     IniManager* config = new IniManager();
-    FileWatcher* fw = new FileWatcher{"./assets/", 3.0f};   
+    FileWatcher* fw = new FileWatcher{"./assets/", 3.0f};
+
+     
+
+
 
     const int windowWidth = GAME_SCREEN_W * config->size;
     const int windowHeight = GAME_SCREEN_H * config->size;
